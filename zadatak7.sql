3a76ce756d4a42dd82b2d3ac1ef1b8b002242016
iivansic <ivan.ivansic@sdfgroup.com>
2020-05-29 22:36:21 +0200

    pocetak rjesavaja zadatak7 s visual studio

---
+++ zadatak7.sql
@@ -0,0 +1,23 @@
+#c:\xampp\mysql\bin\mysql.exe -uedunova -pedunova < D:\pp21\vjezbanje\zadatak7.sql
+#Zadaci za kolokvij_vjezba_7 (u zagradi je maksimalan broj bodova) 
+ #0. Kreirajte tablice (16) i veze između tablica. (6) 
+drop database if exists zadatak7;
+create database zadatak7;
+use zadatak7;
+
+
+ 
+#1. U tablice prijateljica, ostavljen i zarucnik_mladic unesite po 3 retka. (7) 
+ 
+#2. U tablici punica postavite svim zapisima kolonu eura na vrijednost 15,77. (4) 
+ 
+#3. U tablici sestra obrišite sve zapise čija je vrijednost kolone hlace manje od AB. (4) 
+ 
+#4. Izlistajte kratkamajica iz tablice ostavljen uz uvjet da vrijednost kolone introvertno nepoznate. (6) 
+ 
+#5. Prikažite narukvica iz tablice mladic, stilfrizura iz tablice sestra te gustoca iz tablice prijateljica 
+#uz uvjet da su vrijednosti kolone introvertno iz tablice ostavljen poznate te da su vrijednosti kolone 
+#asocijalno iz tablice zarucnik poznate. Podatke posložite po gustoca iz tablice prijateljica silazno. (10) 
+ 
+#6. Prikažite kolone asocijalno i modelnaocala iz tablice zarucnik čiji se primarni ključ ne nalaze u tablici zarucnik_mladic. (5)
+
