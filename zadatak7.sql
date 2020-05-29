7c6e1878c5178a29ee2de7ae20c55c0268440384
iivansic <ivan.ivansic@sdfgroup.com>
2020-05-29 23:37:59 +0200

    testiranje commit iz terminala visual studio

--- zadatak7.sql
+++ zadatak7.sql
@@ -5,8 +5,74 @@ drop database if exists zadatak7;
 create database zadatak7;
 use zadatak7;
 
+create table punica(
+    sifra int not null primary key auto_increment,
+    majica varchar(40),
+    eura decimal(12,6) not null,
+    prstena int,
+    cura int not null
+);
+create table cura(
+    sifra int not null primary key auto_increment,
+    lipa decimal(12,9) not null,
+    introvertno boolean,
+    modelnaocala varchar(40),
+    narukvica int,
+    treciputa datetime,
+    kuna decimal(14,9)
+);
+create table sestra(
+    sifra int not null primary key auto_increment,
+    bojakose varchar(34) not null,
+    hlace varchar(36) not null,
+    lipa decimal(15,6),
+    stilfrizura varchar(40) not null,
+    maraka decimal(12,8) not null,
+    prijateljica int
+);
+create table prijateljica(
+    sifra int not null primary key auto_increment,
+    haljina varchar(45),
+    gustoca decimal(15,10) not null,
+    ogrlica int,
+    novcica decimal(12,5),
+    ostavljen int
+);
+ create table ostavljen(
+    sifra int not null primary key auto_increment,
+    lipa decimal(14,6),
+    introvertno boolean not null,
+    kratkamajica varchar(38) not null,
+    prstena int not null,
+    zarucnik int
+);
+create table zarucnik(
+    sifra int not null primary key auto_increment,
+    vesta varchar(34),
+    asocijalno boolean not null,
+    modelnaocala varchar(43),
+    narukvica int not null,
+    novcica decimal(15,5) not null
+);
+create table zarucnik_mladic(
+    sifra int not null primary key auto_increment,
+    zarucnik int not null,
+    mladic int not null
+);
+create table mladic(
+    sifra int not null primary key auto_increment,
+    prstena int,
+    lipa decimal(14,5) not null,
+    narukvica int not null,
+    drugiputa datetime not null
+);
+alter table zarucnik_mladic add foreign key (mladic) references mladic(sifra);
+alter table zarucnik_mladic add foreign key (zarucnik) references zarucnik(sifra);
+alter table ostavljen add foreign key (zarucnik) references zarucnik(sifra);
+alter table prijateljica add foreign key (ostavljen) references ostavljen(sifra);
+alter table sestra add foreign key (prijateljica) references prijateljica(sifra);
+alter table punica add foreign key (cura) references cura(sifra);
 
- 
 #1. U tablice prijateljica, ostavljen i zarucnik_mladic unesite po 3 retka. (7) 
  
 #2. U tablici punica postavite svim zapisima kolonu eura na vrijednost 15,77. (4) 
