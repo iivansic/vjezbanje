0efadc00fd69ba7f620881d93ecc0309a4e81e65
iivansic <ivan.ivansic@sdfgroup.com>
2020-06-08 22:24:11 +0200

    rješavanje zadataka1-3 sve u jednom file vjezba

---
+++ nemaproblema.hr/koky.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vjezbanje</title>
+</head>
+<?php
+echo '<body style = "background-color:' , $_GET['boja'], ';">'
+?>
+<?php
+echo '<div style="width:100%;text-align:center">'
+?>
+<ul>
+    <li>
+        <a target="_blank" href="index.php"> index</a>
+    </li>
+</ul>
+<?php
+echo "<p style='color: orange' >Ivan Ivanšić </p>";
+?>
+<img src="https://picsum.photos/201"/>
+<img src="https://picsum.photos/302"/>
+<img src="https://picsum.photos/153"/>
+<img src="https://picsum.photos/254"/>
+<img src="https://picsum.photos/355"/>
+<img src="https://picsum.photos/206"/>
+<img src="https://picsum.photos/307"/>
+<img src="https://picsum.photos/158"/>
+<img src="https://picsum.photos/250"/>
+<img src="https://picsum.photos/350"/>
+<img src="https://picsum.photos/221"/>
+<img src="https://picsum.photos/322"/>
+<img src="https://picsum.photos/123"/>
+<img src="https://picsum.photos/224"/>
+<img src="https://picsum.photos/325"/>
+<img src="https://picsum.photos/226"/>
+<img src="https://picsum.photos/300"/>
+<img src="https://picsum.photos/150"/>
+<img src="https://picsum.photos/250"/>
+<img src="https://picsum.photos/320"/>
+<img src="https://picsum.photos/210"/>
+<img src="https://picsum.photos/340"/>
+<img src="https://picsum.photos/160"/>
+<img src="https://picsum.photos/270"/>
+<img src="https://picsum.photos/380"/>
+<img src="https://picsum.photos/290"/>
+<img src="https://picsum.photos/310"/>
+<img src="https://picsum.photos/120"/>
+<img src="https://picsum.photos/230"/>
+<img src="https://picsum.photos/340"/>
+
+</div>
+</body>
+</html>
+
+
+
+
+
